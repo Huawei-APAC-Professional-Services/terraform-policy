a3b84ceda64047c13ba52a9a022d3e9ba2f714e7
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2024-07-26 17:21:58 +0800

    update

---
+++ huaweicloud/unrestrictedport22access.py
@@ -0,0 +1,9 @@
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from securitygroups import UnRestrictedIngressTraffic
+class SecurityGroupUnrestrictedIngress22(UnRestrictedIngressTraffic):
+    def __init__(self):
+        super().__init__(check_id="CKV_HWC_NET_1", port=22)
+    
+
+check = SecurityGroupUnrestrictedIngress22()
