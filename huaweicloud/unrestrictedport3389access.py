ca7affb7a5859be1120957c6b6297e0951173265
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2024-07-26 18:02:37 +0800

    update

--- huaweicloud/unrestrictedport3389access.py
+++ huaweicloud/unrestrictedport3389access.py
@@ -1,6 +1,6 @@
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
-from securitygroups import UnRestrictedIngressTraffic
+from base.securitygroups import UnRestrictedIngressTraffic
 class SecurityGroupUnrestrictedIngress3389(UnRestrictedIngressTraffic):
     def __init__(self):
         super().__init__(check_id="CKV_HWC_NET_2", port=3389)
